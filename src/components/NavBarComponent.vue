ba7878b2244e253e8d1297f65d9e1bccc48b67f7
JuanJoNavC <jjnavarrete008@gmail.com>
2025-06-11 18:02:17 -0500

    NavTest

--- src/components/NavBarComponent.vue
+++ src/components/NavBarComponent.vue
@@ -3,6 +3,51 @@
 </script>
 
 <template>
-    
-
+    <nav class="navContainer">
+        <div class="logo">
+            <i class="fa-brands fa-slack"></i>
+            <img src="" alt="Logo" />
+            <span>Chocolush</span>
+        </div>
+        <ul>
+            <li><router-link to="/">Home</router-link></li>
+            <li><router-link to="/about">About</router-link></li>
+            <li><router-link to="/contact">Contact</router-link></li>
+            <li><router-link to="/login">Login</router-link></li>
+        </ul>
+    </nav>
 </template>
+
+<style scoped>
+.navContainer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 4em;
+}
+
+.navContainer .logo {
+    display: flex;
+    align-items: center;
+    font-size: 1.5em;
+    color: #333;
+}
+.navContainer .logo i {
+    padding: 10px;
+}
+.navContainer ul {
+    font-size: 1em;
+    display: flex;
+    align-items: center;
+    list-style: none;
+    padding: 20px;
+}
+.navContainer li {
+    margin-right: 20px;
+}
+.navContainer a {
+    text-decoration: none;
+    color: #333;
+}
+
+</style>
--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -1,10 +1,14 @@
 <script setup>
-
+import NavBarComponent from '../components/NavBarComponent.vue';
 </script>
 
 <template>
-    <div class="container">
-        <h1>Index</h1>
+    <div>
+        <NavBarComponent />
+        <div class="container">
+            <h1>Welcome to Chocolush</h1>
+            <p>Your one-stop shop for all things chocolate!</p>
+        </div>
     </div>
 
 </template>
